9619eb132af0bce46424eb9f8b8f8eb8d7a0df1a
Lucky Phan <lpt.lucky@gmail.com>
2023-05-10 15:52:02 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-//express app to static files and giving file path to where static files are
-//express doesnt load static automaticcally so needs to be explicitely loaded and its not generated
-//by the server
+// //express app to static files and giving file path to where static files are
+// //express doesnt load static automaticcally so needs to be explicitely loaded and its not generated
+// //by the server
 app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000, () => console.log("Listening on Port 4000"))
